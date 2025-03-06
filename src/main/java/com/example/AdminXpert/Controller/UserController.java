5f4f08f956dba084ce11d0904f54ce0a8be4f94c
Raghavendra-Aprameya <1rn21cs030.aprameya@gmail.com>
2025-03-06 14:33:49 +0530

    feat:Spring security implemented and pushed

--- src/main/java/com/example/AdminXpert/Controller/UserController.java
+++ src/main/java/com/example/AdminXpert/Controller/UserController.java
@@ -1,4 +1,23 @@
 package com.example.AdminXpert.Controller;
 
+import com.example.AdminXpert.Entity.Users;
+import com.example.AdminXpert.Services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auth")
 public class UserController {
+
+    @Autowired
+    UserService userService;
+    @PostMapping("/signup")
+    public Users register(@RequestBody Users user)
+    {
+        return userService.register(user);
+    }
 }
--- src/main/java/com/example/AdminXpert/Services/UserService.java
+++ src/main/java/com/example/AdminXpert/Services/UserService.java
@@ -1,4 +1,23 @@
 package com.example.AdminXpert.Services;
 
+import com.example.AdminXpert.Entity.Users;
+import com.example.AdminXpert.Repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
+    @Autowired
+    UserRepository userRepository;
+
+
+    private BCryptPasswordEncoder encoder=new BCryptPasswordEncoder(12);
+
+    public Users register(Users user){
+        user.setPassword(encoder.encode(user.getPassword()));
+        return userRepository.save(user);
+
+
+    }
 }
